1e1657f8161ced6289ec760f41516ec859e227ab
Regis A. Despres <regis.despres@gmail.com>
2020-04-30 11:17:55 +0200

    typo on patch

--- sysutils/beats7/files/patch-vendor_github.com_insomniacslk_dhcp_dhcpv4_bindtodevice_bsd.go
+++ sysutils/beats7/files/patch-vendor_github.com_insomniacslk_dhcp_dhcpv4_bindtodevice_bsd.go
@@ -1,14 +1,14 @@
 +++ vendor/github.com/insomniacslk/dhcp/dhcpv4/bindtodevice_bsd.go     2020-04-30 08:47:05.639979000 +0000
 @@ -0,0 +1,18 @@
-+// +build freebsd openbsd netbsd
-+-
++// build freebsd openbsd netbsd
++
 +package dhcpv4
-+-
++
 +import (
 +      "net"
 +      "syscall"
 +)
-+-
++
 +// BindToInterface emulates linux's SO_BINDTODEVICE option for a socket by using
 +// IP_RECVIF.
 +func BindToInterface(fd int, ifname string) error {
